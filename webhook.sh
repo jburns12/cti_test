2c0ef51a6eee75b8ad19c8dc6a93307ca34cc6d7
J Burns <burnsjennifere@gmail.com>
2018-07-06 13:19:23 -0400

    Update webhook.sh

--- webhook.sh
+++ webhook.sh
@@ -4,4 +4,4 @@ curl -s -X POST -H "Content-Type: application/json" \
 -H "Accept: application/json" -H "Travis-API-Version: 3" \
 -H "Authorization: token ${TRAVIS_TOKEN}" \
 -d '{"request":{"branch":"master"}}' \
-https://api.travis-ci.org/repo/#REPO_NAME_SPACE#%2F#REPO_NAME#/requests
+https://api.travis-ci.org/repo/jburns12%2fweb_test/requests
