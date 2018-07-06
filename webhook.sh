db86bbc55d69c4cc11213d84afd15830f4b7bef2
J Burns <jburns@mitre.org>
2018-07-06 15:04:08 -0400

    Update webhook.sh

--- webhook.sh
+++ webhook.sh
@@ -3,5 +3,5 @@
 curl -s -X POST -H "Content-Type: application/json" \
 -H "Accept: application/json" -H "Travis-API-Version: 3" \
 -H "Authorization: token ${TRAVIS_TOKEN}" \
--d '{"request":{"branch":"master"}}' \
+-d '{"request":{"branch":"build"}}' \
 https://api.travis-ci.com/repo/jburns12%2fjburns12.github.io/requests
